6f56f1cce0a1b95330908b0bf49a352e80836751
cantelerai <vieira.raiany@escola.pr.gov.br>
2025-10-08 16:11:22 -0300

    Delete docs directory

--- docs/assets/js/config.js
+++
@@ -1,6 +0,0 @@
-// /assets/js/config.js
-
-window.APP_CONFIG = {
-  SUPABASE_URL: "https://yusrnlnxereyagpkhwxn.supabase.co",
-  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl1c3JubG54ZXJleWFncGtod3huIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTkzMTUwOTUsImV4cCI6MjA3NDg5MTA5NX0.sJkImOXIEYRfrPEB6J0ijIaqyors9luO58EIqCYPz_I"
-};
